0ea43dc4b453d2b94ea2ee67d36bc1b36639e43d
Pranav Kumar <pranavkumar2025.pmd@gmail.com>
2024-08-02 23:07:45 +0530

    New commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,26 +1,71 @@
-"use client"
+"use client";
 import React, { useRef, useState } from "react";
 import Header from "./Header";
 import { Validate } from "@/utils/Validate";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "@/utils/firebase";
 
 const Login = () => {
   const [isSignIN, SetisSignIN] = useState(true);
-  const [ErrorMessage,SetErrorMessage]= useState("");
+  const [ErrorMessage, SetErrorMessage] = useState("");
 
   const Email = useRef(null);
   const Password = useRef(null);
 
-  const CheckSignIN = ()=>{
+  const CheckSignIN = () => {
     SetisSignIN(!isSignIN);
-  }
+  };
 
-  const SubmitInput= ()=>{
-    const ValidateResult = Validate(Email.current.value, Password.current.value);
+  const SubmitInput = () => {
+    const ValidateResult = Validate(
+      Email.current.value,
+      Password.current.value
+    );
+
+    const email = Email.current.value;
+    const password = Password.current.value;
     // console.log(Email.current.value)
     // console.log(Password.current.value);
     // console.log(ValidateResult);
     SetErrorMessage(ValidateResult);
-  }
+
+    if (ValidateResult) return;
+
+    if (!isSignIN) {
+      // Sign Up logic
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Signed up
+          const user = userCredential.user;
+          console.log(user);
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          SetErrorMessage(errorCode + "-" + errorMessage);
+          // ..
+        });
+    } else {
+      // Sign In logic
+      signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user);
+          alert("Sign In Succesfull");
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          SetErrorMessage(errorCode + " " + errorMessage)
+        });
+    }
+  };
 
   return (
     <div>
@@ -32,19 +77,21 @@ const Login = () => {
         />
       </div>
       <form
-        onSubmit={(e)=>e.preventDefault()}
+        onSubmit={(e) => e.preventDefault()}
         action=""
         className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-md bg-opacity-80 "
       >
-        <h1 className="text-3xl font-bold p-2">{isSignIN ? "Sign In" : "Sign Up"}</h1>
+        <h1 className="text-3xl font-bold p-2">
+          {isSignIN ? "Sign In" : "Sign Up"}
+        </h1>
         {!isSignIN && (
           <input
-          type="text"
-          name=""
-          id=""
-          placeholder="Full Name"
-          className="p-4 my-2 w-full bg-gray-700 rounded-sm border "
-        />
+            type="text"
+            name=""
+            id=""
+            placeholder="Full Name"
+            className="p-4 my-2 w-full bg-gray-700 rounded-sm border "
+          />
         )}
         <input
           ref={Email}
@@ -63,8 +110,17 @@ const Login = () => {
           className="p-4 my-2 w-full bg-gray-700 rounded-sm border"
         />
         <p className="text-red-600">{ErrorMessage}</p>
-        <button className="p-2 my-4 text-white w-full bg-red-700 rounded-md" onClick={SubmitInput}>{isSignIN ? "Sign In" : "Sign Up"}</button>
-        <h1 onClick={CheckSignIN} className="cursor-pointer">{isSignIN ? "New to Netflix ? Sign Up Now": "Already Registerd ? Sign In Now"}</h1>
+        <button
+          className="p-2 my-4 text-white w-full bg-red-700 rounded-md"
+          onClick={SubmitInput}
+        >
+          {isSignIN ? "Sign In" : "Sign Up"}
+        </button>
+        <h1 onClick={CheckSignIN} className="cursor-pointer">
+          {isSignIN
+            ? "New to Netflix ? Sign Up Now"
+            : "Already Registerd ? Sign In Now"}
+        </h1>
       </form>
     </div>
   );
